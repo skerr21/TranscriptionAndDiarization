c3f255a94752ef62942af736d88fb774719e3e7f
skerr21 <sirkerr@googlemail.com>
2023-07-21 16:34:16 -0500

    added double summarization, chunk and overall using different summarization methods

--- summarization.py
+++ summarization.py
@@ -1,15 +1,18 @@
 from transformers import pipeline
 
 def summarize_text_file(file_path, max_length=130, min_length=30, do_sample=False):
-    # Initialize the summarizer
-    summarizer = pipeline("summarization", model="pszemraj/pegasus-x-large-book-summary")
-    
+    # Initialize the summarizer for chunk-level summarization
+    chunk_summarizer = pipeline("summarization", model="knkarthick/MEETING_SUMMARY")
+
+    # Initialize the summarizer for overall summarization
+    overall_summarizer = pipeline("summarization", model="pszemraj/long-t5-tglobal-base-16384-book-summary", truncate=True)
+
     # Read the contents of the file
     with open(file_path, 'r') as file:
         text = file.read()
 
     # Tokenize the text to get the tokens
-    tokens = summarizer.tokenizer(text, return_tensors='pt', truncation=False)['input_ids'][0]
+    tokens = chunk_summarizer.tokenizer(text, return_tensors='pt', truncation=False)['input_ids'][0]
 
     # Split the tokens into chunks of max_length
     chunks = [tokens[i:i + 1024] for i in range(0, len(tokens), 1024)]
@@ -18,13 +21,13 @@ def summarize_text_file(file_path, max_length=130, min_length=30, do_sample=Fals
     chunk_summaries = []
     for chunk in chunks:
         # Convert the tokens back to text
-        chunk_text = summarizer.tokenizer.decode(chunk)
+        chunk_text = chunk_summarizer.tokenizer.decode(chunk)
         
         # Check if the chunk_text is not empty
         if chunk_text.strip():
             try:
                 # Summarize the chunk text
-                summary = summarizer(chunk_text, max_length=max_length, min_length=min_length, do_sample=do_sample)
+                summary = chunk_summarizer(chunk_text, max_length=max_length, min_length=min_length, do_sample=do_sample)
                 # Add the summary to the list of summaries
                 chunk_summaries.append(summary[0]['summary_text'])
             except IndexError as e:
@@ -35,7 +38,7 @@ def summarize_text_file(file_path, max_length=130, min_length=30, do_sample=Fals
     all_summaries_text = ' '.join(chunk_summaries)
     
     # Create an overall summary of the chunk summaries
-    overall_summary = summarizer(all_summaries_text, max_length=max_length, min_length=min_length, do_sample=do_sample)
+    overall_summary = overall_summarizer(all_summaries_text, max_length=max_length, min_length=min_length, do_sample=do_sample)
 
     return overall_summary
 
